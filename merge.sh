3e3b6c8065f19f2c8d2af0ff216beff354f2e9d7
Naresh Solanki <Naresh.Solanki@9elements.com>
2022-12-20 11:54:26 +0100

    [merge.sh] Add merge script
    Signed-off-by: Naresh Solanki <Naresh.Solanki@9elements.com>

---
+++ merge.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+PREFIX="origin/dev-5.15"
+LOCAL_PREFIX=$(echo ${PREFIX}|sed "s|origin/||")
+
+if [ -d ".git/sequencer" ]; then
+  echo "FATAL ERROR: Cherry-pick or rebase in progress. ABORTING."
+  exit 1
+fi
+
+git fetch
+git checkout ${PREFIX}_base
+git branch -D ${PREFIX}
+
+BRANCH_CMD="git branch --list"
+if [[ ${PREFIX} == "origin/"* ]]; then
+	BRANCH_CMD+=" -r"
+fi
+
+BRANCHES_TO_MERGE=""
+BRANCHES=$(${BRANCH_CMD} |grep $PREFIX|xargs)
+echo "Branches to merge:"
+for b in ${BRANCHES}; do
+  if [[ "${b}" == "${PREFIX}"* ]]; then
+    if [ "${b}" == "${PREFIX}" ]; then continue; fi
+    if [ "${b}" == "${PREFIX}_base" ]; then continue; fi
+    if [[ "${b}" =~ "${PREFIX}-"[0-9]* ]]; then continue; fi
+    BRANCHES_TO_MERGE="${BRANCHES_TO_MERGE} ${b}"
+    echo "${b}"
+  fi
+done
+echo ""
+git merge --no-edit ${BRANCHES_TO_MERGE}
+git branch -D ${LOCAL_PREFIX}
+git branch ${LOCAL_PREFIX}
+git checkout ${LOCAL_PREFIX}
+echo "**************************************"
+echo "Done merging! Now push it\n"
+echo "**************************************"
+
